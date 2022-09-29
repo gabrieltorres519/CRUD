44d05015135b67a6dea57dd8ea5af1d5800fb63e
unknown <gabrieltorres9909@gmail.com>
2022-09-29 13:10:10 -0500

    Agregando primer vista

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@
 // @babel/cli
 // @babel/preset-env
 
+// npm install express express-handlebars mongoose morgan 
 // npm i @babel/core @babel/cli @babel/preset-env nodemon -D
+
+//Gracias a Babel podemos utilizar esta sintaxis para usar las librerías debido a esto se usa  npx babel-node src/index.js
+import {express} from "express";
+import indexRoutes from '../src/routes/index';
+
+const app = express();
+
+app.use(indexRoutes);
+
+app.listen(3000)
+console.log('Sevidor en puerto',3000)
