cbb58bc81caec2ce7ce8a717eb7e6ba119d15e9b
unknown <gabrieltorres9909@gmail.com>
2022-09-29 12:40:53 -0500

    Indicando que el proyecto usará babel

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//Babel digiere código javascript facilitando escrtura, siilar a nodemon en algunos sentidos
+// npm i @babel/core
+// @babel/cli
+// @babel/preset-env
+
+// npm i @babel/core @babel/cli @babel/preset-env nodemon -D
