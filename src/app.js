dfadd03bb5c1da402d0f0e7392f8696315e6c26d
unknown <gabrieltorres9909@gmail.com>
2022-09-29 13:32:05 -0500

    Funcionando la primer vista

---
+++ src/app.js
@@ -0,0 +1,14 @@
+// npm i @babel/core @babel/cli @babel/preset-env nodemon -D
+
+//Gracias a Babel podemos utilizar esta sintaxis para usar las librerías debido a esto se usa  npx babel-node src/index.js
+import {express} from "express";
+import {exphbs} from 'express-handlebars'
+import indexRoutes from '../src/routes/index';
+
+const app = express();
+
+
+app.use(indexRoutes);
+
+
+export default app
--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@
 // npm i @babel/core
 // @babel/cli
 // @babel/preset-env
+// @babel/node
 
 // npm install express express-handlebars mongoose morgan 
-// npm i @babel/core @babel/cli @babel/preset-env nodemon -D
-
-//Gracias a Babel podemos utilizar esta sintaxis para usar las librerías debido a esto se usa  npx babel-node src/index.js
-import {express} from "express";
+// import app from './app'
+import express from "express";
 import indexRoutes from '../src/routes/index';
 
 const app = express();
 
-app.use(indexRoutes);
 
+app.use(indexRoutes);
 app.listen(3000)
 console.log('Sevidor en puerto',3000)
